a540de34ad39e48b195e0f7ea5813316b80169ba
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-08-20 10:54:53 -0400

    Use the resource generator for Posts

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe 'creation' do
+
+    before do
+      @post = Post.create(date: Time.now, rationale: 'anything')
+    end
+    it 'can be created' do
+
+      expect(@post).to be_valid
+    end
+
+    it 'can not be created without a date and rationale' do
+      @post.date = ' '
+      @post.rationale = nil
+      expect(@post).to_not be_valid
+    end
+  end
+end
